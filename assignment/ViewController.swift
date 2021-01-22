d0b20489cc0da1ed8d0ed95ff96d89953f213c60
MartinMoo <martin.miklass@gmail.com>
2021-01-22 19:50:30 +0100

    Initial Commit

---
+++ assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment
+//
+//  Created by Moo Maa on 22/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
